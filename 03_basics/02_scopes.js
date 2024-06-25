ede102a6af46e7c4777e7e0d142ba50b3055d134
sidhantkaushal <kaushalsidhant434@gmail.com>
2024-06-25 21:48:54 +0530

    scopes done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,12 +1,9 @@
-
-
-if(true){
-    // This is block scope
-    let a=10;
-    const b=20;
-    var c=30;
+if (true) {
+  // This is block scope
+  let a = 10;
+  const b = 20;
+  var c = 30;
 }
-
 
 // console.log(a);
 // console.log(b);
@@ -16,27 +13,32 @@ console.log(c);
 
 // The global scope in browser's console and code environment of nodejs are different.
 
-
 // 21 Done
-
 
 // The smaller can ask for things but the older cannot.
 // This is somewhat closure where the inner function can access information from its outer function.
 
-function one(){
-    const username="sid";
-    function two(){
-        const website="youtube";
-        console.log(username);
-    }
-    //This line gives error as its out of scope.
-    // console.log(website);
-    two();
+function one() {
+  const username = "sid";
+  function two() {
+    const website = "youtube";
+    console.log(username);
+  }
+  //This line gives error as its out of scope.
+  // console.log(website);
+  two();
 }
 
 one();
 
- 
 //+++++++++++++ Interesting +++++++++++++
 
-function addOne (num)
+console.log(addOne(10));
+function addOne(nums) {
+  return nums + 1;
+}
+addTwo(10);
+//This is a functional expression and it gives error if it is called before it is initalized.
+const addTwo = function (nums) {
+  return nums + 2;
+};
